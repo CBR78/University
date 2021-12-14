db5739a44399feb8aa4c4c7b27af18082db4ffd2
Vladimir Malzam <v.malzam@gmail.com>
2021-12-14 20:16:38 +0300

    Use for l18n message-autoconfiguration

--- src/main/java/com/cbr/university/UniversityWebApp.java
+++ src/main/java/com/cbr/university/UniversityWebApp.java
@@ -3,9 +3,7 @@ package com.cbr.university;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -17,6 +15,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 public class UniversityWebApp extends SpringBootServletInitializer implements WebMvcConfigurer {
+
     private static final String INDEX = "index";
 
     public static void main(String[] args) {
@@ -40,14 +39,5 @@ public class UniversityWebApp extends SpringBootServletInitializer implements We
         CookieLocaleResolver resolver = new CookieLocaleResolver();
         resolver.setDefaultLocale(Locale.US);
         return resolver;
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("i18n/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setDefaultLocale(Locale.US);
-        return messageSource;
     }
 }
