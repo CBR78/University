2ee1dc041c723f54959fb61a3e219a4ab277a299
Vladimir Malzam <v.malzam@gmail.com>
2024-05-09 15:07:13 +0300

    прописан passwordEncoder

--- src/main/java/com/cbr/university/SecurityConfig.java
+++ src/main/java/com/cbr/university/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -46,32 +48,24 @@ public class SecurityConfig {
 //    }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("student").password("1234").roles("STUDENT")
-//                .username("editor").password("4321").roles("EDITOR")
-//                .username("user").password("password2").roles("USER")
-//                .username("rest").password("1122").roles("REST")
-//                .build();
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
-        UserDetails userStudent = User.withDefaultPasswordEncoder()
-                .username("student").password("1234").roles("STUDENT").build();
-        UserDetails userEditor = User.withDefaultPasswordEncoder()
-                .username("editor").password("4321").roles("EDITOR").build();
-        UserDetails userRest = User.withDefaultPasswordEncoder()
-                .username("rest").password("1122").roles("REST").build();
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails userStudent = User
+                .withUsername("student")
+                .password(passwordEncoder.encode("1234")).roles("STUDENT").build();
+        UserDetails userEditor = User
+                .withUsername("editor")
+                .password(passwordEncoder.encode("4321")).roles("EDITOR").build();
+        UserDetails userRest = User
+                .withUsername("rest")
+                .password(passwordEncoder.encode("1122")).roles("REST").build();
 
         return new InMemoryUserDetailsManager(userStudent, userEditor, userRest);
     }
-
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("student").password(encoder.encode("1234")).roles("STUDENT").and()
-//                .withUser("editor").password(encoder.encode("4321")).roles("EDITOR").and()
-//                .withUser("rest").password(encoder.encode("1122")).roles("REST");
-//    }
 
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http
